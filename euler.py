7f19b9e22a1b0e0f62abc88426f68e3e68e8b7b0
Wei-Wei Luo <lww5879@gmail.com>
2014-04-08 19:50:31 +0800

    problem 11 and 12

--- euler.py
+++ euler.py
@@ -1,4 +1,7 @@
 """Helper functions for Project Euler"""
+import math
+import operator
+import itertools
 
 def prime(maximum):
     """Generate sequence of prime numbers up to `maximum`"""
@@ -13,5 +16,30 @@ def prime(maximum):
             if i % j == 0: break
         else:
             # That's a prime!
+            yield i
+
+def nchoosek(n, k):
+    """Returns the binomial coefficient"""
+    fact = math.factorial
+    return fact(n)//fact(k)//fact(n-k)
+
+#   if n < 0 or k < 0:
+#       raise Exception('Non negative number required!')
+#   if n < k:
+#       return 0
+#   return (reduce(operator.mul, (i for i in xrange(n, n-k, -1))) /
+#       reduce(operator.mul, (i for i in xrange(1, k+1))))
+
+def sumofdigits(integer):
+    """Return sum of digits in integer"""
+    assert integer > 0
+    s = str(integer)
+    return sum(int(i) for i in list(s))
+
+def divisor(integer):
+    """Generate divisor sequence of positive int `integer` increasingly"""
+    assert integer > 0
+    for i in range(1, integer+1):
+        if integer % i == 0:
             yield i
 
---
+++ p12.py
@@ -0,0 +1,29 @@
+"""Highly divisible triangular number"""
+
+def triangle_number(n=20000):
+    """Generate first `n` triangle number"""
+    for i in xrange(1, n):
+        yield (i+1)*i/2
+
+
+for n in triangle_number():
+    count = 0
+    for i in xrange(1, n+1):
+        if n % i == 0:
+            j = n / i
+            count += 1
+            if j < i:
+                count = 2 * (count - 1)
+                break
+            elif j == i:
+                count = 2 * count - 1
+                break
+    if count > 500:
+        print 'Done'
+        break
+
+
+print """The first triangle number to have over five hundred divisors ({}) is:
+    {}""".format(count, n)
+
+
